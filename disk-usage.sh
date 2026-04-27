6f4d501d991e5733c8fad1e8da5acc727b0b48f3
Manoj G <manojganesh615@gmail.com>
2026-04-27 11:29:15 +0530

    Modify disk usage script to strip percentage sign
    Remove percentage sign from disk usage result.

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=$(df -h | awk '{print $1,$5}' | grep "/dev/root" | cut -d " " -f 2)
+result=$(df -h | awk '{print $1,$5}' | grep "/dev/root" | cut -d " " -f 2| sed 's/%//')
 
 if [ $result -ge 75 ];
 then
