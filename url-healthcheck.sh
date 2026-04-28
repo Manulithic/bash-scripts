9ab2c107916e771266414b95be87f400c961ab78
Manoj G <manojganesh615@gmail.com>
2026-04-28 11:44:17 +0530

    Fix curl output format for HTTP status code

--- url-healthcheck.sh
+++ url-healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-status_code=$(curl -o /dev/null -s -w "{%http_code}" $1)
+status_code=$(curl -o /dev/null -s -w "%{http_code}" $1)
 # $1 represents first command line argument. we will be passing the healthcheck url as first command line argument
 
 if [ "$status_code" -eq 200 ];
